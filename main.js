4c78b1aa1452a58c363bc091dcc2fb18f2cda1dc
Natalya Myunster <natalyamyunster@yahoo.com>
2020-01-24 12:48:27 +0100

    added setTimeout

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const body = document.body;
-const page = document.getElementById('page');
-const preloader = document.createElement('div');
-// preloader.src = "http://www.picshare.ru/view/10391702/";
-preloader.className = 'preloader-spinner';
-body.replaceChild(preloader, page);
 
-//setTimeout(() => body.replaceChild(page, preloader), 2000);
+const page = document.getElementById('page');
+const preloader = document.querySelector('.preloader');
+preloader.style.display = 'none';
+
+ const onClick = ()  => {
+     preloader.style.display = 'block';
+     page.style.display = 'none';
+ };
+
+
+ page.addEventListener('click', onClick);
+
+setTimeout(() => {
+    preloader.style.display = 'none';
+    page.style.display = 'block';
+    }, 2000);
