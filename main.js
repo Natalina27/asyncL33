89dd108d91e28fdf020588cb27e3fb08dfee6831
Natalya Myunster <natalyamyunster@yahoo.com>
2020-01-24 12:58:19 +0100

    added setTimeout

--- main.js
+++ main.js
@@ -3,15 +3,23 @@ const page = document.getElementById('page');
 const preloader = document.querySelector('.preloader');
 preloader.style.display = 'none';
 
+
+
+
  const onClick = ()  => {
-     preloader.style.display = 'block';
-     page.style.display = 'none';
+     if (preloader.style.display === 'none'){
+         preloader.style.display = 'block';
+         page.style.display = 'none';
+     } else{
+     preloader.style.display = 'none';
+         page.style.display = 'block';
+     }
  };
 
-
- page.addEventListener('click', onClick);
 
 setTimeout(() => {
     preloader.style.display = 'none';
     page.style.display = 'block';
     }, 2000);
+
+page.addEventListener('click', onClick);
