a84a21f42fa23f6780e929c991903e96bd88e613
Natalya Myunster <natalyamyunster@yahoo.com>
2020-01-24 16:06:06 +0100

    removed button

--- main.js
+++ main.js
@@ -6,20 +6,4 @@ body.replaceChild(preloader, page);
 setTimeout(() => body.replaceChild(page, preloader), 2000);
 
 
-// preloader.style.display = 'none';
-//
-//
-//
-//
-//   const onClick = ()  => {
-//       preloader.style.display = 'block';
-//       page.style.display = 'none';
-//  };
-//
-// button.addEventListener('click', onClick);
-//
-// setTimeout(() => {
-//     preloader.style.display = 'none';
-//     page.style.display = 'block';
-//     }, 2000);
 
